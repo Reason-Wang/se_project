d25e963518d141ada0b3c1b705717f56698f9e0b
lvhaodong <1720866841@qq.com>
2022-04-28 18:31:20 +0800

    init_MERCHAN table

--- StoreManage/Merchandise.py
+++ StoreManage/Merchandise.py
@@ -1,64 +1,90 @@
-"""
-Merchandise类 为商品的数据结构
-使用数据库中的表MERCHAN来存储商品信息
+# Merchandise类 为商品的数据结构
+# 使用数据库中的表MERCHAN来存储商品信息
 
 
-    MERCHAN表的定义:
-        CREATE TABLE MERCHAN(
-            ID INT PRIMARY KEY,
-            NAME VARCHAR(30),
-            PRICE INT,
-            NUMBER INT 
-        )
+#     MERCHAN表的定义:
+#         CREATE TABLE IF NOT EXISTS MERCHAN(
+#             ID INT PRIMARY KEY,
+#             NAME VARCHAR(30),
+#             PRICE INT,
+#             NUMBER INT 
+#         )
 
 
-todo
-为新商品获取唯一标识 & 在数据库中的存储 (初步想法 ：SERIAL)
-"""
+# todo
+# 为新商品获取唯一标识 & 在数据库中的存储 (初步想法 ：SERIAL)
 
 
+
+from random import random
 import sys
-sys.path.append('../utils')
-import db_utils
+sys.path.append('..')
+from utils.db_utils import *
+
 
 
 class Merchandise:
-    def __init__(self):
-        self.id=-1          #商品唯一标识
-        self.name=""        #商品名称
-        self.price=-1       #商品价格
-        self.number=-1      #商品库存量
+    def __init__(self,id,name,price,number):
+        self.id=id          #商品唯一标识
+        self.name=name        #商品名称
+        self.price=price       #商品价格
+        self.number=number      #商品库存量
 
+    def toCell(self):
+        return (self.id,self.name,self.price,self.number)
 
 
 class Merchandise_Manage:
-    def __init(self):
+    def __init__(self):
         self.user=""
         self.db_user="lhd"
         self.passwd="123456"
         self.tablename="MERCHAN"
-        merchandise_list=Get_merchandise_list()
+        self.merchandise_list=self.get_merchandise_list()
 
 
-    def toCell(self):
-        return (self.name,self.price,self.number)
+    #only for init table
+    def init_MERCHAN_table(self):
+        connection = create_connection(db_name='se',db_user=self.db_user,db_password=self.passwd)
 
+        create_MERCHAN_table = """
+            CREATE TABLE IF NOT EXISTS MERCHAN(
+            ID INT PRIMARY KEY,
+            NAME VARCHAR(30),
+            PRICE INT,
+            NUMBER INT 
+            )
+        """
+        execute_query(connection,create_MERCHAN_table)
 
-    def Get_merchandise_list():
-        mer_list=[]
-        ##todo get merchandise_list from database
-        return mer_list
-
+        origin_list=[]
+        for i in range(10):
+            id=i
+            name = "name_" + str(i)
+            price = int(random()*100)
+            number = 100
+            origin_list.append(Merchandise(i,name,price,number))
+        for item in origin_list:
+            insert_origin_info = f"""
+            INSERT INTO MERCHAN (ID,NAME,PRICE,NUMBER)
+                VALUES {item.toCell()}
+            """
+            execute_query(connection,insert_origin_info)
+        print("insert done")
+        
+        close(connection)
+    
+    def get_merchandise_list(self):
+        connection = create_connection(db_name='se',db_user=self.db_user,db_password=self.passwd)
+        select_all_from_MERCHAN="""
+            SELECT * FROM MERCHAN
+        """
+        result = execute_read_query(connection,select_all_from_MERCHAN)
+        close(connection)
+        return result
 
 if __name__=='__main__':
-    posts = [
-        ("Happy", "I am feeling very happy today", 1),
-        ("Hot Weather", "The weather is very hot today", 2),
-        ("Help", "I need some help with my work", 2),
-        ("Great News", "I am getting married", 1),
-        ("Interesting Game", "It was a fantastic game of tennis", 5),
-        ("Party", "Anyone up for a late-night party today?", 3),
-    ]
+    manager = Merchandise_Manage()
+    print(manager.merchandise_list)
 
-    post_records = ", ".join(["%s"] * len(posts))
-    print(post_records)
+
